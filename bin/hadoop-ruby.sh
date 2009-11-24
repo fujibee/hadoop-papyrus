9d93d6d566f55b6b5304794451d20c072c5162f0
Koichi Fujikawa <fujibee@gmail.com>
2009-11-25 01:38:36 +0900

    arrange to run with hadoop in hudson.

--- bin/hadoop-ruby.sh
+++ bin/hadoop-ruby.sh
@@ -1,11 +1,12 @@
 BIN_DIR=`dirname "$0"`
 BASE_DIR=`cd $BIN_DIR/..; pwd`
 
+# choose hadoop sh
 HADOOP=$HADOOP_HOME/bin/hadoop 
 if [ ! -f $HADOOP ]; then
   HADOOP=$BIN_DIR/hadoop
+  HADOOP_OPTS="-conf conf/hadoop-site.xml"
 fi
-HADOOP_RUBY_LIB_DIR=$BASE_DIR/lib
 
 # fetch jruby jar if not exist
 LIB_DIR=$BASE_DIR/lib/java
@@ -16,10 +17,12 @@ if [ ! -f "$LIB_DIR/$JRUBY_JAR" ]; then
 fi
 
 # construct command line
+HADOOP_RUBY_LIB_DIR=$BASE_DIR/lib
 for x in `ls $HADOOP_RUBY_LIB_DIR`; do
   DSL_FILES=$HADOOP_RUBY_LIB_DIR/$x,$DSL_FILES
 done
 DSL_FILES=$DSL_FILES$1
 
+# execute hadoop ruby
 echo runnig $1...
-$HADOOP jar lib/java/hadoop-ruby.jar org.apache.hadoop.ruby.JRubyJobRunner -libjars lib/java/jruby-complete-*.jar -files $DSL_FILES $1 $2 $3
+$HADOOP jar $HADOOP_OPTS $LIB_DIR/hadoop-ruby.jar org.apache.hadoop.ruby.JRubyJobRunner -libjars $LIB_DIR/$JRUBY_JAR -files $DSL_FILES $1 $2 $3
