a92d20b304234497afc29f48980b7e639a157ef6
Koichi Fujikawa <fujibee@gmail.com>
2010-01-21 00:35:33 +0900

    prevent reading dsl file in map/reduce by file cachning into memory.

--- lib/util.rb
+++ lib/util.rb
@@ -2,21 +2,37 @@
 require 'hadoop_dsl'
 
 module HadoopDsl
+  # file body cache
+  # reading file in map/reduce cause critical issues!
+  @@file_bodies = {}
+
   def self.snake_case(str)
     str.gsub(/\B[A-Z]/, '_\&').downcase
   end
 
   def self.read_file(file_name)
+    # use if cached
+    body = @@file_bodies[file_name] if @@file_bodies[file_name]
+
     # read as usual
-    body = File.open(file_name).read rescue nil
-    return body if body
+    body = File.open(file_name).read rescue nil unless body
 
     # read from loadpath
-    $:.each do |path|
-      body = File.open(File.join(path, file_name)).read rescue next
-      return body if body
+    unless body
+      $:.each do |path|
+        body = File.open(File.join(path, file_name)).read rescue next
+        break
+      end
     end
 
-    raise "cannot find file - #{file_name}"
+    raise "cannot find file - #{file_name}" unless body
+
+    # for cache
+    @@file_bodies[file_name] = body
+    body
+  end
+
+  def self.reset_dsl_file
+    @@file_bodies = {}
   end
 end
--- spec/util_spec.rb
+++ spec/util_spec.rb
@@ -2,17 +2,33 @@ require File.join(File.dirname(__FILE__) , 'spec_helper')
 require 'util'
 
 describe 'utilities' do
+  before do
+    HadoopDsl.reset_dsl_file
+    @script_body = 'This is a script body.'
+    @script = create_tmp_script(@script_body)
+  end
+
   it 'can change camelcase str to snakecase' do
     HadoopDsl.snake_case('CamelCaseStr').should == 'camel_case_str'
   end
 
   it 'can read file and get file data to string' do
-    script_body = 'This is a script body.'
-    @script = create_tmp_script(script_body)
-    HadoopDsl.read_file(@script).should == script_body
+    HadoopDsl.read_file(@script).should == @script_body
   end
 
   it 'raise error if no file in loadpath' do
     lambda { HadoopDsl.read_file('not_exists_on_loadpath') }.should raise_error
+  end
+
+  it 'can load from cache if script is loaded' do
+    HadoopDsl.read_file(@script).should == @script_body
+    File.delete(@script)
+    HadoopDsl.read_file(@script).should == @script_body
+  end
+
+  it 'can load from each cache even if one script is loaded' do
+    HadoopDsl.read_file(@script).should == @script_body
+    another_script = create_tmp_script("another")
+    HadoopDsl.read_file(another_script).should == "another"
   end
 end
